82ad1a7027ca6c9e605cc6216e47b2e6c70ff2e8
Arseniy <arseniyparakhnych11@gmail.com>
2026-08-17 21:18:36 +0300

    Initiate project

---
+++ Tictactoe/TictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TictactoeApp.swift
+//  Tictactoe
+//
+//  Created by Arseniy on 17.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct TictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
